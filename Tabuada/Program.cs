b6969e9c185f09cff7eb8e72092c751f9b4ca6f5
Luan-art <luanlf2003@gmail.com>
2024-04-18 16:03:06 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,17 @@
+﻿//Faça um programa que leia um numero inteiro positivo e imprima a sua tabuada
+int numero = 0, aux = 0;
+Console.WriteLine("Digite um numero para descobrirmos sua tabuada: ");
+numero = int.Parse(Console.ReadLine());
+aux = numero;
+if (numero > 0)
+{
+     for(int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine($" {aux} vezes {i} = {aux * i}");
+    }
+}
+else
+{
+    Console.WriteLine("numero invalido");
+}  
+
